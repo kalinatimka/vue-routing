12377f5323a378427cabf2845b36aa0a1b665d42
Tsimafei Kalinouski <Tsimafei_Kalinouski@epam.com>
2020-02-25 15:18:49 +0300

    feat: add routing to the project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheNavigation/>
+    <router-view :key="$route.path"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheNavigation from './components/TheNavigation';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    TheNavigation,
   }
 }
 </script>
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav id="nav">
+    <p class="logo">The Vue School Travel App</p>
+    <ul class="nav-links">
+      <li class="links">
+        <router-link to="/">Home</router-link>
+      </li>
+      <li v-for="destination in destinations" :key="destination.name" class="links">
+        <router-link :to="{name: 'DestinationDetails', params: {id: destination.id}}">
+          {{destination.name}}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import store from '../store.js';
+
+export default {
+  data() {
+    return {
+      destinationId: this.$route.params.id,
+      destinations: store.destinations,
+    }
+  }
+}
+</script>
+
+<style scoped>
+#nav {
+  display: flex;
+  align-items: center;
+}
+
+#nav a {
+  color: #2c3e50;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.nav-links {
+  display: flex;
+  list-style: none;
+}
+
+.links {
+  padding-right: 20px;
+}
+
+.links:hover {
+  text-decoration: underline;
+}
+
+.logo {
+  font-size: 20px;
+  color: #42b983;
+}
+</style>
